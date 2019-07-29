d10f56856a656ee515bd0ddcfaba43ad10b70814
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2019-07-29 10:24:07 +0300

    hpack

--- src/Data/Convert/Instances/OverloadedLabels.hs
+++ src/Data/Convert/Instances/OverloadedLabels.hs
@@ -10,5 +10,5 @@ import Data.Convert.Instances.Text
 import qualified Data.Text      as Strict
 import qualified Data.Text.Lazy as Lazy
 
-instance KnownSymbol s => IsLabel s Strict.Text where fromLabel _ = convert $ symbolVal (Proxy :: Proxy s) ; {-# INLINE fromLabel #-}
-instance KnownSymbol s => IsLabel s Lazy.Text   where fromLabel _ = convert $ symbolVal (Proxy :: Proxy s) ; {-# INLINE fromLabel #-}
+instance KnownSymbol s => IsLabel s Strict.Text where fromLabel = convert $ symbolVal (Proxy :: Proxy s) ; {-# INLINE fromLabel #-}
+instance KnownSymbol s => IsLabel s Lazy.Text   where fromLabel = convert $ symbolVal (Proxy :: Proxy s) ; {-# INLINE fromLabel #-}
